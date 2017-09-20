6156f06b6e27a1df646a3a5b8cdb30eef7d31983
Administrator <Administrator@wxd-thinkpad>
2017-09-20 23:09:42 +0800

    test

--- src/helloworld/HelloWorld.java
+++ src/helloworld/HelloWorld.java
@@ -3,7 +3,7 @@ package helloworld;
 public class HelloWorld {
 
 	public static void main(String[] args) {
-		System.out.println("Hello world !!!");
+		System.out.println("Hello  ¿ΩÁ !!!");
 	}
 
 }
